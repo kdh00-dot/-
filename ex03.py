f84afed4a7da61c5c08c4f25fffa886934132ee1
kdh00-dot <kdh3787@gmail.com>
2026-06-28 22:52:43 +0900

    Add files via upload

---
+++ ex03.py
@@ -0,0 +1,21 @@
+print(type("안녕하세요"))
+    # <class 'str'> 'str'은 문자열(string)이라는 뜻입니다. 
+    # 문자열은 글자, 단어, 문장 등을 나타내는 데이터 타입입니다.
+print(type(2233))
+    # <class 'int'> 'int'은 정수(integer)라는 뜻입니다. 
+    # 정수는 소수점이 없는 숫자를 나타내는 데이터 타입입니다.
+print(type(3.14))
+    # <class 'float'> 'float'은 부동 소수점(floating point)이라는 뜻입니다. 
+    # 부동 소수점은 소수점을 포함한 숫자를 나타내는 데이터 타입입니다.
+print(type(True))
+    # <class 'bool'> 'bool'은 불리언(boolean)이라는 뜻입니다. 
+    # 불리언은 참(True) 또는 거짓(False)을 나타내는 데이터 타입입니다.
+print(type([1, 2, 3]))
+    # <class 'list'> 'list'은 리스트(list)라는 뜻입니다. 
+    # 리스트는 여러 개의 값을 순서대로 저장할 수 있는 데이터 타입입니다.
+print(type((1, 2, 3)))
+    # <class 'tuple'> 'tuple'은 튜플(tuple)이라는 뜻입니다. 
+    # 튜플은 여러 개의 값을 순서대로 저장할 수 있는 데이터 타입이지만, 리스트와 달리 수정할 수 없습니다.
+print(type({"name": "Alice", "age": 30}))
+    # <class 'dict'> 'dict'은 사전(dictionary)이라는 뜻입니다. 
+    # 사전은 키와 값의 쌍으로 이루어진 데이터 타입입니다.
