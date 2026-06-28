a0897c58d0c6ebf1f2c7aa60e8f5523ab72675d7
kdh00-dot <kdh3787@gmail.com>
2026-06-28 23:03:33 +0900

    Add files via upload

---
+++ ex03.py
@@ -0,0 +1,9 @@
+print(type("안녕하세요"))
+    # <class 'str'> 'str'은 문자열(string)이라는 뜻입니다. 
+    # 문자열은 글자, 단어, 문장 등을 나타내는 데이터 타입입니다.
+print(type(2233))
+    # <class 'int'> 'int'은 정수(integer)라는 뜻입니다. 
+    # 정수는 소수점이 없는 숫자를 나타내는 데이터 타입입니다.
+print(type(3.14))
+    # <class 'float'> 'float'은 부동 소수점(floating point)이라는 뜻입니다. 
+    # 부동 소수점은 소수점을 포함한 숫자를 나타내는 데이터 타입입니다.
